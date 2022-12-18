3b3b5a1f9d6e89cd706d9db73e15717853041fde
BuildTools <unconfigured@null.spigotmc.org>
2022-12-18 20:39:57 +0100

    test auto-update

--- src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
+++ src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
@@ -10,6 +10,8 @@ import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 
 public class ServeurManageurUpdate {
 
@@ -18,38 +20,62 @@ public class ServeurManageurUpdate {
         try {
         	URL url = new URL("https://github.com/SISIG48/ServeurManageur/archive/refs/heads/main.zip");
             ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-            FileOutputStream fos = new FileOutputStream("plugins\\ServeurManageur.jar");
+            FileOutputStream fos = new FileOutputStream("plugins\\ServeurManageur.zip");
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
             fos.close();
             rbc.close();
-            File file = new File("plugins\\ServeurManageur.jar");
-            FileInputStream fis = new FileInputStream(file);
-            ZipInputStream zis = new ZipInputStream(fis);
-            ZipEntry zipEntry;
-            while ((zipEntry = zis.getNextEntry()) != null) {
-               if (zipEntry.getName().matches("ServeurManageur-main.*")) {
-                  File destFile = new File("plugins\\ServeurManageur.jar");
-                  fos = new FileOutputStream(destFile);
-                  byte[] bytes = new byte[1024];
-                  int length;
+            FileInputStream fin = new FileInputStream("plugins\\ServeurManageur.zip");
+            ZipInputStream zin = new ZipInputStream(fin);
+            ZipEntry ze = null;
 
-                  while ((length = zis.read(bytes)) >= 0) {
-                     fos.write(bytes, 0, length);
-                  }
-
-                  fos.close();
-               }
+            while ((ze = zin.getNextEntry()) != null) {
+                if(ze.isDirectory()) {
+                    File f = new File(ze.getName());
+                    f.mkdirs();
+                } else {
+                    FileOutputStream fout = new FileOutputStream(ze.getName());
+                    for (int c = zin.read(); c != -1; c = zin.read()) {
+                        fout.write(c);
+                    }
+                    zin.closeEntry();
+                    fout.close();
+                }
             }
+            zin.close();
 
-            zis.close();
+            // Compression
+
+            String source = "plugins\\ServeurManageur\\ServeurManageur-main";
+            String destination = "ServeurManageur.jar";
+
+            FileOutputStream fout = new FileOutputStream(destination);
+            ZipOutputStream zout = new ZipOutputStream(fout);
+
+            File f = new File(source);
+            File[] files = f.listFiles();
+
+            for(int i=0; i < files.length; i++) {
+                FileInputStream fin2 = new FileInputStream(files[i]);
+                zout.putNextEntry(new ZipEntry(files[i].getName()));
+                int length;
+                while((length = fin2.read()) > 0) {
+                    zout.write(length);
+                }
+                zout.closeEntry();
+                fin2.close();
+            }
+            zout.close();
+            File delta = new File("ServeurManageur-main");
+            delta.delete();
+            delta = new File("plugins\\ServeurManageur.zip");
+            delta.delete();
             return true;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
-    
-    public static Boolean CheckUpdate() {
+   public static Boolean CheckUpdate() {
     	try {
         	URL url = new URL("https://github.com/SISIG48/ServeurManageur/archive/refs/heads/main.zip");
             ReadableByteChannel rbc = Channels.newChannel(url.openStream());
@@ -69,5 +95,5 @@ public class ServeurManageurUpdate {
 	    	return false;
 	    }
     }
-
 }
+
