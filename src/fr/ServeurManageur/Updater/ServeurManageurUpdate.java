e3ef5c8480adeb2a36106724ac24d9261a007a4e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-19 12:04:02 +0100

    fix auto-update

--- src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
+++ src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
@@ -1,13 +1,18 @@
 package fr.ServeurManageur.Updater;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -24,76 +29,115 @@ public class ServeurManageurUpdate {
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
             fos.close();
             rbc.close();
-            FileInputStream fin = new FileInputStream("plugins\\ServeurManageur.zip");
-            ZipInputStream zin = new ZipInputStream(fin);
+            
+            
+            File zipfile = new File("plugins\\ServeurManageur.zip");
+            File folder = new File("plugins\\");
+            ZipInputStream zis = new ZipInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(zipfile.getCanonicalFile())));
+
+            // extractions des entrées du fichiers zip (i.e. le contenu du zip)
             ZipEntry ze = null;
+            try {
+                while((ze = zis.getNextEntry()) != null){
 
-            while ((ze = zin.getNextEntry()) != null) {
-                if(ze.isDirectory()) {
-                    File f = new File(ze.getName());
-                    f.mkdirs();
-                } else {
-                    FileOutputStream fout = new FileOutputStream(ze.getName());
-                    for (int c = zin.read(); c != -1; c = zin.read()) {
-                        fout.write(c);
+                    // Pour chaque entrée, on crée un fichier
+                    // dans le répertoire de sortie "folder"
+                    File f = new File(folder.getCanonicalPath(), ze.getName());
+               
+                    // Si l'entrée est un répertoire,
+                    // on le crée dans le répertoire de sortie
+                    // et on passe à l'entrée suivante (continue)
+                    if (ze.isDirectory()) {
+                        f.mkdirs();
+                        continue;
                     }
-                    zin.closeEntry();
-                    fout.close();
+                   
+                    // L'entrée est un fichier, on crée une OutputStream
+                    // pour écrire le contenu du nouveau fichier
+                    f.getParentFile().mkdirs();
+                    OutputStream fot = new BufferedOutputStream(
+                            new FileOutputStream(f));
+               
+                    // On écrit le contenu du nouveau fichier
+                    // qu'on lit à partir de la ZipInputStream
+                    // au moyen d'un buffer (byte[])
+                    try {
+                        try {
+                            final byte[] buf = new byte[8192];
+                            int bytesRead;
+                            while (-1 != (bytesRead = zis.read(buf)))
+                                fot.write(buf, 0, bytesRead);
+                        }
+                        finally {
+                            fot.close();
+                        }
+                    }
+                    catch (final IOException ioe) {
+                        // en cas d'erreur on efface le fichier
+                        f.delete();
+                        throw ioe;
+                    }
                 }
             }
-            zin.close();
-
-            // Compression
-
-            String source = "ServeurManageur-main";
-            String destination = "plugins\\ServeurManageur.jar";
-
-            FileOutputStream fout = new FileOutputStream(destination);
-            ZipOutputStream zout = new ZipOutputStream(fout);
-
-            File f = new File(source);
-            File[] files = f.listFiles();
-
-            for(int i=0; i < files.length; i++) {
-                FileInputStream fin2 = new FileInputStream(files[i]);
-                zout.putNextEntry(new ZipEntry(files[i].getName()));
-                int length;
-                while((length = fin2.read()) > 0) {
-                    zout.write(length);
-                }
-                zout.closeEntry();
-                fin2.close();
+            finally {
+                // fermeture de la ZipInputStream
+                zis.close();
             }
-            zout.close();
-            File delta = new File("ServeurManageur-main");
+
+            String sourceDir = "plugins\\ServeurManageur-main\\";
+            String zipFile = "plugins\\ServeurManageur-main\\ServeurManageur-main.zip";
+            fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            addDirToZipArchive(zos, sourceDir, null);
+            zos.close();
+            fos.close();
+            File delta = new File("plugins\\ServeurManageur-main");
             delta.delete();
-            delta = new File("plugins\\ServeurManageur.zip");
-            delta.delete();
+            
             return true;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
-   public static Boolean CheckUpdate() {
-    	try {
-        	URL url = new URL("https://github.com/SISIG48/ServeurManageur/archive/refs/heads/main.zip");
-            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-            FileOutputStream fos = new FileOutputStream("ServeurManageur.jar"); 
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            fos.close();
-            rbc.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    
+    public static void addDirToZipArchive(ZipOutputStream zos, String sourceDir, String baseDir) throws IOException {
+        File srcFile = new File(sourceDir);
+        String[] fileNames = srcFile.list();
+        if (fileNames != null && fileNames.length > 0) {
+            for (String fileName : fileNames) {
+                File file = new File(sourceDir + File.separator + fileName);
+                if (baseDir == null) {
+                    baseDir = srcFile.getName();
+                }
+                if (file.isDirectory()) {
+                    String dir = baseDir + File.separator + file.getName();
+                    zos.putNextEntry(new ZipEntry(dir + File.separator));
+                    addDirToZipArchive(zos, file.getAbsolutePath(), dir);
+                } else {
+                    zos.putNextEntry(new ZipEntry(baseDir + File.separator + file.getName()));
+                    byte[] bytes = Files.readAllBytes(file.toPath());
+                    zos.write(bytes, 0, bytes.length);
+                    zos.closeEntry();
+                }
+            }
         }
-    	File delta = new File("ServeurManageur.jar");
-    	if (!(Paths.get("plugins\\ServeurManageur.jar").toAbsolutePath().equals(Paths.get("ServeurManageur.jar").toAbsolutePath()))) {
-    		delta.delete();
-	    	return true;
-	    } else {
-	    	delta.delete();
-	    	return false;
-	    }
     }
+    public static Boolean CheckUpdate() {
+    	try {
+    		URL url = new URL("https://github.com/SISIG48/ServeurManageur/archive/refs/heads/main.zip");
+            byte[] remoteFileBytes = Files.readAllBytes(Paths.get("plugins\\ServeurManageur.zip"));
+            byte[] localFileBytes = url.openStream().readAllBytes();
+            if (Arrays.equals(remoteFileBytes, localFileBytes)) {
+            	return false;
+            }
+    	} catch (Exception e) {
+    	    e.printStackTrace();
+    	}
+	    return true;
+    }
+
 }
 
