33ec19f610d3a1d90c873ee755cc7ecf8c4d4df3
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 17:56:23 +0100

    ?

--- src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
+++ src/fr/ServeurManageur/Updater/ServeurManageurUpdate.java
@@ -2,7 +2,6 @@ package fr.ServeurManageur.Updater;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
