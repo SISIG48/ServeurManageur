725d708c82247ef69f088e5a9e011bb9d7b69fb0
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 18:49:38 +0100

    ?

--- src/fr/sisig48/pl/Command/CommandeRe.java
+++ src/fr/sisig48/pl/Command/CommandeRe.java
@@ -15,17 +15,41 @@ public class CommandeRe implements CommandExecutor {
 				sender.sendMessage("§aDetecteur auto de maj : §e" + String.valueOf(ServeurManageurUpdate.NeedUpdate));
 				return true;
 			}
+			if(e.equalsIgnoreCase("reload")) {
+				sender.sendMessage("§aDetecteur auto de maj : §eSTART");
+				if(ServeurManageurUpdate.CheckUpdate()) {
+					sender.sendMessage("§eYou need do §4UPDATE");
+					ServeurManageurUpdate.NeedUpdate = 1;
+				} else {
+					sender.sendMessage("§eYou dont need §aUPDATE");
+					ServeurManageurUpdate.NeedUpdate = 0;
+				}
+				return true;
+			}
+			if(e.equalsIgnoreCase("forceMaj")) {
+				sender.sendMessage("§eVerrUpdate Syetem say : §4You dont need §aUPDATE");
+				sender.sendMessage("§eVerrUpdate Syetem say : §4Your §aUPDATE §4starting...");
+				if (!sender.isOp()) {sender.sendMessage("§4You need are OP to execute this command"); return false;}
+				if(ServeurManageurUpdate.DoUpdate(sender)) {sender.sendMessage("§eVerrUpdate Syetem say : §4Your §aUPDATE §4ending | §aSUCCESS"); return true;}
+				sender.sendMessage("§eVerrUpdate Syetem say : §4Your §aUPDATE §4ending | §4FAILED"); 
+				return false;
+			}
 		}
-		if(sender instanceof Player) {
-			Player player = (Player) sender;
-			if (!player.hasPermission("DEFAULT_PERMISSION")) {player.sendMessage("§4You need are OP to execute this command"); return false;}
-			if(ServeurManageurUpdate.DoUpdate(sender)) {player.sendMessage("Maj effectuer"); return true;}
-			player.sendMessage("Maj non effectuer"); 
-			return false;
-		} else {
-			if(ServeurManageurUpdate.DoUpdate(sender)) {System.out.println("Maj effectuer"); return true;}
-			System.err.println("Maj non effectuer");
-			return false;
+		
+		if(ServeurManageurUpdate.NeedUpdate == 1) {
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+				if (!player.isOp()) {player.sendMessage("§4You need are OP to execute this command"); return false;}
+				if(ServeurManageurUpdate.DoUpdate(sender)) {player.sendMessage("Maj effectuer"); return true;}
+				player.sendMessage("Maj non effectuer"); 
+				return false;
+			} else {
+				if(ServeurManageurUpdate.DoUpdate(sender)) {System.out.println("Maj effectuer"); return true;}
+				System.err.println("Maj non effectuer");
+				return false;
+			}
 		}
+		sender.sendMessage("§eVerrUpdate Syetem say : §4You dont need §aUPDATE");
+		return true;
 	}
 }
